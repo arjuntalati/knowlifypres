f8b3fa003e5757af789cc7ed40058250eeb65a18
arjuntalati <arjun.talati@ufl.edu>
2024-07-20 19:38:38 -0400

    basic presentation

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import GoogleSlides from './GoogleSlides';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ritams Goated Presentation</h1>
+        <p>Watch him cook</p>
+        <GoogleSlides />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/GoogleSlides.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const GoogleSlides = () => {
+  return (
+    <div className="GoogleSlides">
+      <iframe 
+        src="https://docs.google.com/presentation/d/1pPIo5BHEMzddglMhYOi1aNHqpGwhRMCCnJVfD5yBaK4/embed?start=true&loop=true&delayms=3000" 
+        frameBorder="0" 
+        width="960" 
+        height="569" 
+        allowFullScreen="true"
+        title="Ritams Goated Presentation"
+      ></iframe>
+    </div>
+  );
+}
+
+export default GoogleSlides;
